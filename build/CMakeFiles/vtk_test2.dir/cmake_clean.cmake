ea0c13fdf22607a9ccc0527fbc614e6023ea1d96
Zadock <malobazadock@gmail.com>
2021-03-13 08:24:21 +0300

    Finally got a cone to render on the screen.

---
+++ build/CMakeFiles/vtk_test2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vtk_test2.dir/main.cpp.o"
+  "vtk_test2"
+  "vtk_test2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vtk_test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+#include <vtk-7.1/vtkAutoInit.h> 
+VTK_MODULE_INIT(vtkRenderingOpenGL2); // VTK was built with vtkRenderingOpenGL2
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+#include <iostream>
+#include <vtk-7.1/vtkActor.h>
+#include <vtk-7.1/vtkCamera.h>
+#include <vtk-7.1/vtkCylinderSource.h>
+#include <vtk-7.1/vtkConeSource.h>
+#include <vtk-7.1/vtkNamedColors.h>
+#include <vtk-7.1/vtkNew.h>
+#include <vtk-7.1/vtkPolyDataMapper.h>
+#include <vtk-7.1/vtkProperty.h>
+#include <vtk-7.1/vtkRenderWindow.h>
+#include <vtk-7.1/vtkRenderWindowInteractor.h>
+#include <vtk-7.1/vtkRenderer.h>
+#include <array>
+
+int main(int argc, char **argv) 
+{
+  vtkConeSource* cone = vtkConeSource::New();
+  cone->Update();
+
+  vtkPolyDataMapper* mapper = vtkPolyDataMapper::New();
+  mapper->SetInputConnection(cone->GetOutputPort());
+
+  vtkActor* conActor = vtkActor::New();
+  conActor->SetMapper(mapper);
+
+  vtkRenderer* ren = vtkRenderer::New();
+  ren->AddActor(conActor);
+
+  vtkRenderWindow* win = vtkRenderWindow::New();
+  win->AddRenderer(ren);
+
+  vtkRenderWindowInteractor* winIntr = vtkRenderWindowInteractor::New();
+  winIntr->SetRenderWindow(win);
+
+  win->Render();
+  winIntr->Start();
+
+  return EXIT_SUCCESS;
 }
